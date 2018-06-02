723b8d60453eb315f1b26c9929246b45947d08c8
DESKTOP-HLKGEMR\Admin <phungthaouit@gmail.com>
2018-06-02 10:31:23 +0700

    - Add tasks and add fonts.

--- src/components/TaskDoing.js
+++ src/components/TaskDoing.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 class TaskDoing extends Component {
 
     render() {
+        var { task } = this.props;
         return (
             <li className="list-group-item">
-                <p className="task">Login fail with email</p>
-                <span className="author"><span className="glyphicon glyphicon-user" aria-hidden="true" />&nbsp;admin@gmail.com</span>
+                <p className="task">{ task.name }</p>
+                <span className="author"><span className="glyphicon glyphicon-user" aria-hidden="true" />&nbsp;{ task.email }</span>
                 <button type="button" className="btn btn-warning btn-xs">Completed</button>
             </li>
         );
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 
+import { tasksRef } from '../firebase';
+import { v4 as uuid } from 'uuid';
+
 class TaskForm extends Component {
 
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: ''
+        };
+    }
 
+    handleOnSubmit = e => {
+        e.preventDefault();
+        var { name } = this.state;
+        tasksRef.push({
+            id: uuid(),
+            name,
+            email: 'phungthaouit@gmail.com',
+            status: 0
+        });
+        this.setState({
+            name: ''
+        });
+    }
+
+    handleChange = e => {        
+        var { name, value } = e.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+        var { name } = this.state;
+        
         return (
             <div className="panel-footer text-right">
-                <form className="form-inline">
+                <form className="form-inline" onSubmit={this.handleOnSubmit}>
                     <div className="form-group">
-                        <input type="text" name="task" className="form-control" placeholder="Task" />
+                        <input type="text" value={ name } onChange={this.handleChange} name="name" className="form-control" placeholder="Task" />
                     </div>
                     <button type="submit" className="btn btn-info">Add</button>
                 </form>
