9d7a28d630de6d9fb34341ab5e9e56b33162bb88
Phung Minh Tran <phung.tran@amblique.com phung.tran@amblique.com>
2018-06-11 13:59:14 +0700

    - Delete task finish.

--- src/components/TaskFinish.js
+++ src/components/TaskFinish.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
+import { tasksRef }         from '../firebase';
+
 class TaskFinish extends Component {
+
+    handleOnClick = key => {
+        tasksRef.child(key).remove();
+    }
 
     render() {
         var { task } = this.props;
@@ -8,6 +14,7 @@ class TaskFinish extends Component {
             <li className="list-group-item">
                 <p className="task">{ task.name }</p>
                 <span className="author"><span className="glyphicon glyphicon-user" aria-hidden="true" />&nbsp;{ task.email }</span>
+                <button onClick={() => this.handleOnClick(task.key)} type='button' className='btn btn-danger btn-xs'>Delete</button>
             </li>
         );
     }
